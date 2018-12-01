191d5d474f51d750220bcb417af24210b95ebb72
Aleksandr <aleksandr.v.dikarev@gmail.com>
2018-12-01 22:47:25 +0400

    Create AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UnderwaterVideo")]
+[assembly: AssemblyDescription("For educational purposes. Underwater video transmitter/receiver demo application. Credits: Alek Dikarev, Stas Dmitriev, Vitaliy Kubkin")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Underwater communication & navigation laboratory, LLC")]
+[assembly: AssemblyProduct("UnderwaterVideo")]
+[assembly: AssemblyCopyright("Copyright © 2011-2018, Alek Dikarev, Stanislav Dmitriev, Vitaliy Kubkin")]
+[assembly: AssemblyTrademark("UC&NL")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76aae12b-702e-4264-97fc-ff97971a28d4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
